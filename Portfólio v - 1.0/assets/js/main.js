c83963bca82b6b3437adc1df4faff452c8fd7b20
Lucas Santos <santosdacamaralucas1@gmail.com>
2023-07-26 23:02:18 -0300

    Initial commit

---
+++ Portfólio v - 1.0/assets/js/main.js
@@ -0,0 +1,45 @@
+// NavBar
+class MobileNavbar {
+    constructor(navbar, navList, navLinks) {
+      this.navbar = document.querySelector(navbar);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.navbar.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.navbar.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.navbar) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".menu-responsive",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
